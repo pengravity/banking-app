7d81aebd5ac3db4d9f299870106ef5e6d787bff5
Andy <alipinski@mail.com>
2022-10-28 17:17:21 +0200

    display account transactions

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Data
 const account1 = {
   owner: 'John Smith',
-  movements: [1200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [1500, 1450, -400, 3000, -1650, -130, 500, 1300],
   interestRate: 1.5, // %
   pin: 1111,
 };
@@ -26,8 +26,8 @@ const account3 = {
 
 const account4 = {
   owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
+  movements: [1200, -500, 3340, -500, -220, -50, 1400, -360],
+  interestRate: 1.2,
   pin: 4444,
 };
 
@@ -58,3 +58,22 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+//
+const displayMovements = movements => {
+  containerMovements.innerHTML = '';
+
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = ` 
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">${mov}</div>
+  </div>
+  `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
